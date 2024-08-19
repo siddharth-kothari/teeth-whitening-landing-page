4389559bc20ffb79b1c8806db64f5afad3827e41
Siddharth Kothari <sidkothari005@gmail.com>
2024-08-19 15:55:21 +0530

    Updated Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,20 +3,29 @@
 import { headerLogo } from "@/assets";
 import Image from "next/image";
 import Link from "next/link";
-
 import React, { useState } from "react";
-import {
-  HoveredLink,
-  Menu,
-  MenuItem,
-  ProductItem,
-} from "@/components/ui/navbar-menu";
 import { cn } from "@/lib/utils";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
 const Header = () => {
   const [active, setActive] = useState<string | null>(null);
+  const components: { title: string; href: string }[] = [
+    {
+      title: "Teeth Whitening",
+      href: "/treatments/teeth-whitening",
+    },
+  ];
   return (
-    <header className="sticky top-0 z-10 bg-blur">
+    <header className="pt-5 pb-3 sticky top-0 z-10 bg-blur">
       <div className="container px-4 mx-auto sm:px-6 lg:px-8">
         <div className="flex items-center justify-between">
           <div className="flex-shrink-0">
@@ -44,34 +53,38 @@ const Header = () => {
             </button>
           </div>
 
-          <Menu setActive={setActive}>
-            <MenuItem
-              setActive={setActive}
-              active={active}
-              item="About Us"
-              isSubMenu={false}
-              href="/about-us"
-            ></MenuItem>
-            <MenuItem
-              setActive={setActive}
-              active={active}
-              item="Treatments"
-              isSubMenu={true}
-            >
-              <div className="grid grid-cols-3 gap-10 p-2">
-                <HoveredLink href="/treatments/teeth-whitening">
-                  Teeth Whitening
-                </HoveredLink>
-              </div>
-            </MenuItem>
-            <MenuItem
-              setActive={setActive}
-              active={active}
-              item="Contact Us"
-              isSubMenu={false}
-              href="/contact-us"
-            ></MenuItem>
-          </Menu>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <Link href="/about-us" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    About Us
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Treatments</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:grid-cols-3 lg:w-[600px] ">
+                    {components.map((component) => (
+                      <ListItem
+                        key={component.title}
+                        title={component.title}
+                        href={component.href}
+                      ></ListItem>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/contact-us" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Contact Us
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
 
           <div className="hidden lg:flex lg:items-center lg:space-x-8 xl:space-x-10">
             <a
@@ -88,5 +101,28 @@ const Header = () => {
     </header>
   );
 };
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
 
 export default Header;
