d2aa97e11c0faa164043d6925851735070e9e455
Siddharth Kothari <sidkothari005@gmail.com>
2024-08-19 13:28:54 +0530

    Treatments page v1

---
+++ app/treatments/teeth-whitening/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Teeth Whitening | Dental Care Solutions",
+  description:
+    "Dental Care Solutions offers comprehensive dental services, including advanced teeth whitening treatments, to enhance your smile. Our expert team provides personalized care in a comfortable and welcoming environment.",
+};
+
+const Treatments = () => {
+  return <div>Teeth Whitening</div>;
+};
+
+export default Treatments;
