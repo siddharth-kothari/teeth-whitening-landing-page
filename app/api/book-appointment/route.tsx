df27fa246427c4c7cdd4ff70f640d8eb4a710487
Siddharth Kothari <sidkothari005@gmail.com>
2024-08-21 20:19:16 +0530

    Booking Appointment email to user and API changes

--- app/api/book-appointment/route.tsx
+++ app/api/book-appointment/route.tsx
@@ -1,21 +1,73 @@
 import { NextResponse } from "next/server";
 import { query } from "@/config/db";
+import { getServerSession } from "next-auth";
+import { options as authOptions } from "../auth/[...nextauth]/options";
+import { Resend } from "resend";
+import { BookAppointmentEmail } from "@/emails/index";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const POST = async (req: Request) => {
   const data = await req.json();
-  console.log(data);
+
+  // Fetch the session using the App Directory way
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
+  }
+
+  if (session.user.id != data.user_id) {
+    return NextResponse.json(
+      { message: "Unauthorised Access" },
+      { status: 403 }
+    );
+  }
+
   try {
     const result = await query({
       query:
-        "INSERT INTO appointments(user_id, appointment_date, appointment_time,  comments) VALUES(?,?,?,?)",
+        "INSERT INTO appointments(user_id, appointment_date, appointment_time, comments) VALUES(?,?,?,?)",
       data: [data.user_id, data.date, data.selectedTimeSlot, data.comments],
     });
+
+    const [user] = await query({
+      query: "SELECT first_name FROM users WHERE id = ?",
+      data: [data.user_id],
+    });
+
+    console.log("result", result.insertId);
+    console.log("user", user.first_name);
+
+    if (result.insertId) {
+      await resend.emails.send({
+        from: "Dental Care Solutions <Dental-Care-Solutions@dcs-test.siddharthkothari.com>",
+        to: session.user.email,
+        subject: "Appointment Confirmation at Dental Care Solutions",
+        react: (
+          <BookAppointmentEmail
+            userFirstname={user.first_name}
+            apt_data={data}
+          />
+        ),
+      });
+    }
 
     return NextResponse.json(
       { message: "Request Sent.", status: 200 },
       { status: 200 }
     );
   } catch (error) {
-    return NextResponse.json({ message: error, status: 500 }, { status: 500 });
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { message: error.message, status: 500 },
+        { status: 500 }
+      );
+    } else {
+      return NextResponse.json(
+        { message: "An unknown error occurred", status: 500 },
+        { status: 500 }
+      );
+    }
   }
 };
---
+++ emails/index.tsx
@@ -0,0 +1,122 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
+
+interface KoalaWelcomeEmailProps {
+  userFirstname: string;
+  apt_data: any;
+}
+
+const baseUrl = process.env.NEXT_PUBLIC_PROD_URL
+  ? `https://${process.env.NEXT_PUBLIC_PROD_URL}`
+  : "http://localhost:3000";
+
+export const BookAppointmentEmail = ({
+  userFirstname,
+  apt_data,
+}: KoalaWelcomeEmailProps) => (
+  <Html>
+    <Head />
+    <Preview>
+      The sales intelligence platform that helps you uncover qualified leads.
+    </Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Img
+          src={`${baseUrl}/logo.png`}
+          width="100"
+          height="100"
+          alt="Dental Care Solutions"
+          style={logo}
+        />
+        <Text style={paragraph}>Hi {userFirstname},</Text>
+        <Text style={paragraph}>
+          Thank you for scheduling an appointment with Dental Care Solutions. We
+          are pleased to confirm your appointment and look forward to seeing you
+          at {apt_data.selectedTimeSlot} on {apt_data.date}.
+        </Text>
+
+        <Text style={paragraph}>
+          <strong>Location:</strong> 101, Elpis IVF and Maternity Hospital,
+          Parihar Chowk, ITI Rd, beside Malabar Gold and Diamonds, Aundh, Pune,
+          Maharashtra 411007
+        </Text>
+
+        <Text style={paragraph}>
+          If you need to reschedule or have any questions before your visit,
+          please do not hesitate to contact us at{" "}
+          <Link href="tel:+917972659371">+91 79726 59371</Link>
+        </Text>
+
+        <Text style={paragraph}>
+          Best Regards,
+          <br />
+          Dental Care Solutions
+        </Text>
+      </Container>
+    </Body>
+  </Html>
+);
+
+BookAppointmentEmail.PreviewProps = {
+  userFirstname: "Alan",
+} as KoalaWelcomeEmailProps;
+
+export default BookAppointmentEmail;
+
+const main = {
+  backgroundColor: "#ffffff",
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+};
+
+const container = {
+  margin: "0 auto",
+  padding: "20px 0 48px",
+};
+
+const logo = {
+  margin: "0 auto",
+};
+
+const paragraph = {
+  fontSize: "16px",
+  lineHeight: "26px",
+  color: "#000",
+};
+
+const btnContainer = {
+  textAlign: "center" as const,
+};
+
+const button = {
+  backgroundColor: "#5F51E8",
+  borderRadius: "3px",
+  color: "#fff",
+  fontSize: "16px",
+  textDecoration: "none",
+  textAlign: "center" as const,
+  display: "block",
+  padding: "12px",
+};
+
+const hr = {
+  borderColor: "#cccccc",
+  margin: "20px 0",
+};
+
+const footer = {
+  color: "#8898aa",
+  fontSize: "12px",
+};
